3ca136ed2c3dbf8680d37f01d53b2ebb2ff9ab89
NicolasGuerrero <nicoguerrero0800@gmail.com>
2026-07-14 18:26:10 -0600

    feat(rewards): export referral capabilities

---
+++ site-backend/src/rewards/referrals/index.ts
@@ -0,0 +1,8 @@
+/** Customer referral attribution and milestone boundary. */
+export const rewardReferralsBoundary = "reward-referrals" as const;
+
+export * from "./attribution.js";
+export * from "./limit-policy.js";
+export * from "./registration-award.js";
+export * from "./permanence.js";
+export * from "./http-application.js";
